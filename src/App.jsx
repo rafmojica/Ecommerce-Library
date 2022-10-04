105c179bd9b17e4ffacccf3f5dc03304df62d6c7
rafa <rafumojica1@gmail.com>
2022-10-04 20:57:31 +0800

    initialized new repository

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
+import { books } from "./data";
+import BookInfo from "./pages/BookInfo";
+import Cart from "./pages/Cart";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(book) {
+    setCart([...cart, book]);
+  }
+
+  function changeQuantity(book, quantity) {
+    setCart(
+      cart.map((item) =>
+        +item.id === +book.id
+          ? {
+              ...item,
+              quantity: +quantity,
+            }
+          : item
+      )
+    );
+  }
+
+  function removeItem(item) {
+    setCart(cart.filter((book) => book.id !== item.id));
+  }
+
+  useEffect(() => {
+    console.log(cart);
+  }, [cart]);
+
+  return (
+    <Router>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/books" element={<Books books={books} />} />
+          <Route
+            path="/books/:id"
+            element={
+              <BookInfo books={books} addToCart={addToCart} cart={cart} />
+            }
+          />
+          <Route
+            path="/cart"
+            element={
+              <Cart
+                books={books}
+                cart={cart}
+                changeQuantity={changeQuantity}
+                removeItem={removeItem}
+              />
+            }
+          />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/ui/Highlight.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Highlight = ({ icon, title, paragraph }) => {
+  return (
+    <>
+      <div className="highlight">
+        <div className="highlight__img">{icon}</div>
+        <h3 className="highlight__subtitle">{title}</h3>
+        <p className="highlight__para">{paragraph}</p>
+      </div>
+    </>
+  );
+};
+
+export default Highlight;
